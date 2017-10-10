bdbebaba773ba301939adfdb599f1e054e5e3303
MikkoRantakallio <mikko.rantakallio@gmail.com>
2017-10-10 14:12:50 +0300

    File added

---
+++ BankApp/Model/BankAccountHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankApp.Model
+{
+    public class BankAccountHandler
+    {
+        public BankAccountHandler()
+        {
+        }
+
+        public void DeleteBankAccount(int id)
+        {
+            using (var context = new BankdbContext())
+            {
+                var bankAccount = context.BankAccount.Find(id);
+                context.BankAccount.Remove(bankAccount);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteCustomerBankAccount(int custId)
+        {
+            using (var context = new BankdbContext())
+            {
+                var bankAcc = context.BankAccount.Where(bankAccount => bankAccount.CustomerId == custId).FirstOrDefault();
+//                context.BankAccount.Remove(bankAcc);
+                context.SaveChanges();
+            }
+        }
+
+        public List<BankAccount> GetAccountList()
+        {
+            using (var context = new BankdbContext())
+            {
+                var accounts = context.BankAccount.ToList();
+                return accounts;
+            }
+        }
+    }
+}
